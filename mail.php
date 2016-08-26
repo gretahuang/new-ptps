5d2a177b7c62c95c2a48c017d6a95c7e30500fdf
Tatevik Stepanyan <tatevik.stepanyan@berkeley.edu>
2016-08-26 00:03:14 -0700

    revert to test for a sec

--- mail.php
+++ mail.php
@@ -3,11 +3,14 @@ $subject = $_POST["subject"];
 
 
 if ($subject == 'Publicity') {
-    $to = 'ptps.berkeley.publicity@gmail.com';
+    $to = 'tateviks96@gmail.com';
+    // $to = 'ptps.berkeley.publicity@gmail.com';
 } elseif ($subject == 'Site Questions') {
-    $to = 'ptps.berkeley.site@gmail.com';
+    $to = 'johnclara@comcast.net';
+    // $to = 'ptps.berkeley.site@gmail.com';
 } else {
-    $to = 'ptps.berkeley@gmail.com';
+    $to = 'tatevik.stepanyan@berkeley.edu';
+    // $to = 'ptps.berkeley@gmail.com';
 }
 
 $name = $_POST["name"];
